74d291d229eb1e56367be6a1a146b5ada50b90f3
dat nguyen <datnguyen26111997@gmail.com>
2021-09-21 16:04:50 +0700

    add event onClick

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItems';
 
+const data = [
+  { title: 'hello', isComplete: true },
+  { title: 'this is ', isComplete: true },
+  { title: 'test' }
+]
+function onItemClick() {
+  console.log('aa')
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.length > 0 && data.map((item) => <TodoItem item={item} onClick={onItemClick} />)}
+      {data.length === 0 && 'nothing here'}
     </div>
   );
 }
